3701ac41c9842e92de7deae02334932fdf45f5c6
Martín Miadosqui <martinmiadosqui@gmail.com>
2022-07-21 20:56:56 -0300

    Clase 137: Selector Actores

---
+++ PeliculasAPI/DTOS/PeliculasActorDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeliculasAPI.DTOS
+{
+    public class PeliculasActorDTO
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Foto { get; set; }
+    }
+}
